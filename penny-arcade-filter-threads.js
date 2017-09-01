cda0bd948edbef8c5e80e57fc172550c378c43e0
Ken Harris <kharris627@gmail.com>
2017-09-01 12:03:38 -0400

    Adding a button to each thread that will automatically add them to a filter thread list.

--- penny-arcade-filter-threads.js
+++ penny-arcade-filter-threads.js
@@ -1,28 +1,56 @@
 // ==UserScript==
-// @name         Penny-Arcade Filter Thread
+// @name         Penny-Arcade Filter Threads
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Filters any thread out that you wish.
 // @author       urahonky
 // @match        https://forums.penny-arcade.com/*
 // @grant        none
 // ==/UserScript==
 
-// Examples:
-// const ignoreThreadTitlesContaining = ["Overwatch"];
-// const ignoreThreadTitlesContaining = ["Overwatch", "FFRK", "Forza"];
-const ignoreThreadTitlesContaining = [];
-
 (function() {
     'use strict';
 
-    let threads = Array.from(document.getElementsByClassName("Title"));
-
-    ignoreThreadTitlesContaining.forEach(function(ignoreTitle){
-        threads.forEach(function(thread){
-            if(thread.innerHTML.includes(ignoreTitle)){
-                thread.parentNode.parentNode.parentNode.style.display = "none";
+    let threads = Array.from(document.getElementsByClassName("Title")); // Threads on the page
+    let ignoredThreads = localStorage.getItem('hideThreads') ? JSON.parse(localStorage.getItem('hideThreads')) : []; // Grabs thread IDs that have already been clicked, or empty list if none.
+    
+    let clickFunction = function(text, thread){
+        let hiddenThreadStorage = localStorage.getItem('hideThreads');
+        if(hiddenThreadStorage){
+            let hiddenThreads = JSON.parse(hiddenThreadStorage);
+            if(hiddenThreads.indexOf(text) === -1){
+                hiddenThreads.push(text);
+                localStorage.setItem('hideThreads', JSON.stringify(hiddenThreads));
             }
-        });
+        } else {
+            localStorage.setItem('hideThreads', JSON.stringify([text]));
+        }
+        thread.parentNode.parentNode.parentNode.style.display = "none";
+    };
+    
+    let buttonStyle = "float:left; border:none; background:transparent; color:red; font-size:18px; padding-right:15px";
+
+    // Add X to each thread so you can remove them without having to add the titles manually
+    threads.forEach(function(thread){
+        let threadId = thread.getAttribute("href").replace("https://forums.penny-arcade.com/discussion/","").split("/")[0];
+        if(ignoredThreads.indexOf(threadId) !== -1){
+            // This is an ignored thread
+            thread.parentNode.parentNode.parentNode.style.display = "none";
+        } else {
+            let button = document.createElement("button");
+            button.innerHTML = "X";
+            button.setAttribute("style", buttonStyle);            
+
+            // Grab div containing the thread title
+            let threadDiv = thread.parentNode.parentNode;
+            threadDiv.style.paddingTop = "10px";
+            // Grab the div Wrap
+            let divWrap = threadDiv.getElementsByClassName("Wrap")[0];
+            divWrap.style.display = "inline-block";
+            threadDiv.appendChild(button);
+            threadDiv.insertBefore(divWrap, button);
+
+            button.addEventListener("click", clickFunction.bind(this, threadId, thread));            
+        }
     });
 })();
